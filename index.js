1fee5c803dfa00ae66cde5d4b9f2f250c68d127b
Andrey <andreymak2004@gmail.com>
2024-10-03 09:24:11 +1000

    fix

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const bot = new Telegraf(process.env.KEY_CODE)
 bot.start((ctx) => ctx.reply('Здесь ты можешь задать вопрос спикеру'));
 
 bot.on(message('text'), async (ctx) => {
-  // Explicit usage
-
-  console.log(ctx.message)
 
   // if(+process.env.KITAEZA_ID !== ctx.update.message.from.id && +process.env.NARINE_ID !== ctx.update.message.from.id) {
     await bot.telegram.sendMessage(process.env.KITAEZA_ID, ctx.message.text)
